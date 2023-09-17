93eb678856a38a72c54bdfffcace72d2a39c3338
parigasimova <periqasimova.454@gmail.com>
2023-09-17 13:48:22 +0400

    asdfg

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
+let cors = require("cors");
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.type('html').send(html));
+let connection = mysql.createConnection({
+  host: "berzz9slsesve1qrxd7k-mysql.services.clever-cloud.com",
+  user: "uortu7wj3uywsssa",
+  password: "z8iqlcKCaJWO4VU0VgT1",
+  database: "berzz9slsesve1qrxd7k",
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/users", function (req, res) {
+  connection.query("select * from users ", function (err, result, fields) {
+    console.log(err);
+    console.log(result);
+    console.log(fields);
+    res.send(result);
+  });
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.get("/users", function (req, res) {
+  connection.query("select * from users", function (err, result, fields) {
+    res.send(result);
+  });
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+app.get("/users/:id", (req, res) => {
+  const elem = req.params;
+
+  connection.query("select * from users", function (err, result, fields) {
+    for (let i = 0; i < result.length; i++) {
+      if (elem.id == result[i].ID) {
+        res.send(result[i]);
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    }
+  });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const elem = req.params.id;
+  const silininenElementArray = db.filter(
+    (element) => element.ID != elem
+  );
+  connection.query(
+    `DELETE FROM users WHERE ID=${elem}`,
+    function (err, result, fields) {
+      console.log(result);
+    }
+  );
+});
+
+app.post("/users/", (req, res) => {
+  let obj = req.body;
+  connection.query(
+    `INSERT INTO users (ID,name, surname,password)
+          VALUES ("${obj.ID}", "${obj.name}", "${obj.surname}", "${obj.password}")`,
+    function (err, result, fields) {}
+  );
+});
+app.listen(process.env.PORT || 3000);
